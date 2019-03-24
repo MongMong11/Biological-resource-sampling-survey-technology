cb032ec481db07b575cef20b2f34023da3fbd400
MongMong11 <46275621+MongMong11@users.noreply.github.com>
2019-03-25 00:15:36 +0800

    Update ivi_calaulate.R

--- ivi_calaulate.R
+++ ivi_calaulate.R
@@ -1,4 +1,5 @@
 #download data
+
 team3_forest<-'https://raw.githubusercontent.com/MongMong11/Biological-resource-sampling-survey-technology/master/0311_team3-forest%20.csv'
 data <- data.table::fread(team3_forest, encoding = 'UTF-8')
 data<-data.frame(data)
@@ -17,54 +18,59 @@ DataTreeID
 
 # cauculate specie's number - step 2
 
-name<-unique(data[,3])
+Species<-unique(data[,3])
 
 #loop
+
 i=1
 c=c()
-while (i<19) {
-  a<-as.numeric(length(grep(name[i],DataTreeID)))
+while (i<13) {
+  a<-as.numeric(length(grep(Species[i],DataTreeID)))
   c<-append(c,a)
   print(c)
   i=i+1
 }
 
 #result
+
 count<-c
 Den<-count/4
 RD<-Den/sum(Den)*100
+Den_result_data<-cbind(Species, Den, RD)
+as.data.frame(Den_result_data)
 
 # Dominance
 
 Cover_Area<-((data[,4])/2)^2*3.14/100
-Do<-((data[,4])/2)^2*3.14/100
-Do<-c(sum(Do[c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,25)]),
-sum(Do[c(20)]),
-sum(Do[c(21,32)]),
-sum(Do[c(22)]),
-sum(Do[c(23,26)]),
-sum(Do[c(24)]),
-sum(Do[c(27)]),
-sum(Do[c(28)]),
-sum(Do[29]),
-sum(Do[30]),
-sum(Do[31]),
-sum(Do[33]))
-RDo<-Do/sum(Do)*100
+data<-cbind(data, Cover_Area)
+Do<-aggregate(DBH ~ Species, data, sum)
+colnames(Do) <- c("Species", "Do")
+RDo<-Do[,2]/sum(Do[,2])*100
+Do_result_data<-cbind(Do, RDo)
+Do_result_data
+
+# unfinish_result
+
+unfinish_result<-merge(Den_result_data, Do_result_data)
+unfinish_result
 
 # frequency
 
 count2<-c(4,2,3,1,2,1,1,1,2,2,1,1)
 Fr<-count2/4
 RF<-Fr/sum(Fr)*100
+Fr_result_data<-cbind(Species, Fr ,RF)
+Fr_result_data<-as.data.frame(Fr_result_data)
+
+# unfinish_result2
+
+unfinish_result2<-merge(unfinish_result,Fr_result_data)
 
 # ivi
 ivi<-RF+RDo+RD
 sum(ivi)
 
-# make data.frame
-
-ivi_data<-cbind(name, count, Den, RD, Fr, RF, Do, RDo, ivi)
+ivi_data<-cbind(unfinish_result2, ivi)
 ivi_data<-as.data.frame(ivi_data)
 View(ivi_data)
 
@@ -72,6 +78,7 @@ View(ivi_data)
 write.csv(ivi_data, 'C:/Users/user/Desktop/ivi_data.csv', row.names = FALSE)
 
 # calculate DBH class number
+
 data<-as.data.table(data)
 
 DBH_S<-data[,.(DBH.1 = DBH <= 3 ),by=Species]
@@ -84,4 +91,3 @@ DBH_S
 DBH_L<-data[,.(DBH.3= DBH>10),by=Species]
 DBH_L<-as.data.table(DBH_L)
 DBH_L<-DBH_L[DBH.3 %in% c("TRUE")]
-
